e3371001cac7dd9295e229ae915868c4b6098ebc
Saweli Kudasow <skudasov@hotmail.com>
2025-11-13 22:22:37 +0100

    0.1

---
+++ ClickAnim/click_anim.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var anim = $AnimatedSprite2D # получаем компонент анимации
+@onready var building = get_node("/root/Main/Building") # получаем ноду со зданием
+
+func _process(_delta):
+	# если мышка на здании и зажата
+	if building.mouse_over and Input.is_action_pressed("click_left"):
+		anim.global_position = get_global_mouse_position() # анимация перемещается к курсору
+		
+		# показываем и играем анимацию
+		anim.show()
+		anim.play("default")
+	else:
+		# останавливаем и скрываем анимацию
+		anim.stop()
+		anim.hide()
