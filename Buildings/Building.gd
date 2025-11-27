a3e0775386f54ce6d64c9d0149fd33ee6b425975
Saweli Kudasow <skudasov@hotmail.com>
2025-11-27 22:20:43 +0100

    0.5

--- Buildings/Building.gd
+++ Buildings/Building.gd
@@ -13,23 +13,24 @@ func _ready():
 	sprite.texture = normal_texture
 
 func _on_mouse_entered():
-	sprite.texture = hover_texture
+	sprite.texture = hover_texture # поменять на картинку с обводкой
 	Global.mouse_over = self
-	if building_type == "stone":
+	if building_type == "stone": # курсор на фабрике камня
 		upgrade_menu.stone = true
-	if building_type == "castle":
+	if building_type == "castle": # курсор на замке
 		upgrade_menu.castle = true
-	if building_type == "wood":
+	if building_type == "wood": # курсор на фабрике дерева
 		upgrade_menu.wood = true
-	upgrade_menu.show_upgrade()
+	upgrade_menu.show_upgrade() # показать апгрейд меню
 
 func _on_mouse_exited():
-	sprite.texture = normal_texture
+	sprite.texture = normal_texture # назад на обычную картинку
 	if Global.mouse_over == self:
 		Global.mouse_over = null
-	upgrade_menu.hide_upgrade()
+	upgrade_menu.hide_upgrade() # скрыть апгрейд меню
 
 func _process(_delta):
+	# если курсор на здании и зажата лкм начает/останавливает таймер
 	if Global.mouse_over == self and Input.is_action_pressed("click_left"):
 		if not timer.is_stopped():
 			return
@@ -41,8 +42,8 @@ func _process(_delta):
 func _on_timer_timeout():
 	print(building_type)
 	if building_type == "wood":
-		get_node("../UI").add_wood()
+		get_node("../UI").add_wood() # добавляем дерево
 	if building_type == "stone":
-		get_node("../UI").add_stone()
+		get_node("../UI").add_stone() # добавляем камень
 	else:
 		return
--- Upgrade.gd
+++ Upgrade.gd
@@ -1,24 +1,27 @@
 extends Control
 
+# переменные для вычисления ресурса
 var stone := false
 var castle := false
 var wood := false
 
 func _ready():
-	$".".hide()
+	$".".hide() # скрывает меню
 
+# показывает меню апгрейда и перемещает в нужную позицию
 func show_upgrade():
 	$".".show()
-	if stone:
+	if stone: # позиция меню если курсор на фабрике камня
 		position.y = 160
 		position.x = 90
-	if castle:
+	if castle:  # позиция меню если курсор на замке
 		position.y = 160
 		position.x = 176
-	if wood:
+	if wood: # позиция меню если курсор на фабрике дерева
 		position.y = 160
 		position.x = 252
 
+# скрывает меню и сбрасывает переменные всех зданий
 func hide_upgrade():
 	$".".hide()
 	stone = false
