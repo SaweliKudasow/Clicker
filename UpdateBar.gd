84c9ed8cdfc30b4bf70edf60d8cdf8091802904b
Saweli Kudasow <skudasov@hotmail.com>
2025-12-08 22:29:27 +0100

    0.15

--- UpdateBar.gd
+++ UpdateBar.gd
@@ -17,13 +17,29 @@ var bar_upgrades = [
 	Vector2(314, 119)
 ]
 
+var bar_upgrade_anim = [
+	Vector2(368, 65),
+	Vector2(368, 71),
+	Vector2(368, 77),
+	Vector2(368, 83),
+	Vector2(368, 89),
+	Vector2(368, 95),
+	Vector2(368, 101),
+	Vector2(368, 107),
+	Vector2(368, 113),
+	Vector2(368, 119)
+]
+
 var region_size = Vector2(21, 5)
 
 func _ready():
 	rect.texture = rect.texture.duplicate()
 
 func update():
-	#if HoveredBuilding.building == "Castle":
-		#return
 	var level = Upgrades.get_level(HoveredBuilding.building, bar_type)
-	rect.texture.region = Rect2(bar_upgrades[level], region_size)
+	for i in range(3):
+		rect.texture.region = Rect2(bar_upgrade_anim[level - 1], region_size)
+		await get_tree().create_timer(0.1).timeout
+		
+		rect.texture.region = Rect2(bar_upgrades[level], region_size)
+		await get_tree().create_timer(0.1).timeout
