aa39bc652a8b5254cd38325ac32e9f871deed771
Saweli Kudasow <skudasov@hotmail.com>
2025-12-15 21:42:39 +0100

    0.22

--- Reset/ResetButton.gd
+++ Reset/ResetButton.gd
@@ -1,5 +1,31 @@
 extends Control
 
+func _ready():
+	hide_menu()
+	$TextureButton.show()
+
 func _on_texture_button_pressed():
+	play_sound()
+	show_menu()
+
+func _on_no_button_pressed():
+	play_sound()
+	hide_menu()
+
+func _on_yes_button_pressed():
 	GameData.reset()
 	get_tree().reload_current_scene()
+
+func hide_menu():
+	$Question.hide()
+	$ButtonField.hide()
+	$TextureButton.show()
+
+func show_menu():
+	$Question.show()
+	$ButtonField.show()
+	$TextureButton.hide()
+
+func play_sound():
+	$"../ClickSound".stop()
+	$"../ClickSound".play()
